fa19a46982c4bde6b4ab98408e80a8699d8e6675
nmesih <nmesihalan@gmail.com>
2022-08-22 15:52:53 +0300

    added exercises 1 and 2

---
+++ src/weeks/week_02/C02E01.java
@@ -0,0 +1,28 @@
+package weeks.week_02;
+
+import java.util.Scanner;
+
+public class C02E01 {
+
+	/*
+	 *  Write a program that reads a Celsius degree in a double value from the console,
+	 *  then converts it to Fahrenheit and displays the result. 
+	 *  The formula for the conversion is as follows:
+	 *  fahrenheit = (9 / 5) * celsius + 32
+	 */
+
+		public static void main(String[] args) {
+			System.out.println("Enter a degree in Celsius: ");
+			
+			Scanner keyboard = new Scanner(System.in);
+			
+			int Celsius = keyboard.nextInt();
+			
+			double Fahrenheit = (9.0 / 5) * Celsius + 32 ;
+			
+			System.out.println(Celsius + " Celsius " + " is " + Fahrenheit + " Fahrenheit" );
+			
+
+		}
+
+}
---
+++ src/weeks/week_02/C02E02.java
@@ -0,0 +1,40 @@
+package weeks.week_02;
+
+import java.util.Scanner;
+
+public class C02E02 {
+
+	/*
+	 * Write a program that reads in the radius and length of a cylinder 
+	   and computes the area and volume using the following formulas:
+	   
+	   area = radius * radius * PI
+	   volume = area * length
+	   
+	 */
+	
+	public static void main(String[] args) {
+	
+		final double PI = 3.14159;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		double radius;
+		
+		double lenght;
+		
+		
+		System.out.println("Enter the radius and lenght of a cylinder: ");
+		
+		radius = keyboard.nextDouble();
+		lenght = keyboard.nextDouble();
+		
+		double area = radius * radius * PI;
+		double volume = area * lenght;
+		
+		System.out.println("The area is " + area);
+		System.out.println("The volume is  " + volume);
+		
+	}
+
+}
