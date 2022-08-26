fbd1921b98dc3cd75ff8c6f3a91fc9258fafd881
nmesih <nmesihalan@gmail.com>
2022-08-26 12:24:32 +0300

    added c03e01

---
+++ src/chapter/Chapter_03/C03E01.java
@@ -0,0 +1,30 @@
+package chapter.Chapter_03;
+
+import java.util.Scanner;
+
+public class C03E01 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter a, b, c:");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+		
+		double r1 = (-b + (Math.pow(Math.pow(b, 2) - (4 * a * c), 0.5))) / (2 * a);
+		double r2 = (-b - (Math.pow(Math.pow(b, 2) - (4 * a * c), 0.5))) / (2 * a);
+		
+		double discriminant = Math.pow(b, 2) - (4 * a * c);
+		
+		if (discriminant > 0){
+			System.out.println("The equation has two roots " + r1 + " and " + r2);
+		} else if (discriminant == 0) {
+			System.out.println("The equation has one root " + r1 );
+		} else {
+			System.out.println("There are no real roots.");
+		}
+	}
+
+}
