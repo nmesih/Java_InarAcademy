45c5c63712af2077e0684178baf5e1bd271bfc85
nmesih <nmesihalan@gmail.com>
2022-09-02 15:59:00 +0300

    added checkpoints 13 and 15

---
+++ src/chapter/Chapter_04/Checkpoint_13.java
@@ -0,0 +1,14 @@
+package chapter.Chapter_04;
+
+public class Checkpoint_13 {
+
+	public static void main(String[] args) {
+		char x = 'a';
+		char y = 'c';
+		 System.out.println(++x);
+		 System.out.println(y++);
+		 System.out.println(x - y);
+		 }
+	}
+
+
---
+++ src/chapter/Chapter_04/Checkpoint_15.java
@@ -0,0 +1,14 @@
+package chapter.Chapter_04;
+
+public class Checkpoint_15 {
+
+	public static void main(String[] args) {
+		System.out.println('a' < 'b');
+		System.out.println('a' <= 'A');
+		System.out.println('a' > 'b');
+		System.out.println('a' >= 'A');
+		System.out.println('a' == 'a');
+		System.out.println('a' != 'b');
+	}
+
+}
