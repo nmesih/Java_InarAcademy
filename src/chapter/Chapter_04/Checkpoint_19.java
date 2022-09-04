17bfffea948ca2ec4c82639b868019d431455a16
nmesih <nmesihalan@gmail.com>
2022-09-04 23:44:42 +0300

    added checkpoint 19

---
+++ src/chapter/Chapter_04/Checkpoint_19.java
@@ -0,0 +1,12 @@
+package chapter.Chapter_04;
+
+public class Checkpoint_19 {
+
+	public static void main(String[] args) {
+		System.out.println(1 + "Welcome " + 1 + 1);
+		System.out.println(1 + "Welcome " + (1 + 1));
+		System.out.println(1 + "Welcome " + ('\u0001' + 1));
+		System.out.println(1 + "Welcome " + 'a' + 1);
+	}
+
+}
