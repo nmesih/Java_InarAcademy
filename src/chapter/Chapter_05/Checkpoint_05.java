70987ea9d298159eda2e20024a8c12116a7c97b9
nmesih <nmesihalan@gmail.com>
2022-09-07 16:36:49 +0300

    added checkpoint 5

---
+++ src/chapter/Chapter_05/Checkpoint_05.java
@@ -0,0 +1,12 @@
+package chapter.Chapter_05;
+
+public class Checkpoint_05 {
+
+	public static void main(String[] args) {
+		int x = 80000000;
+		while (x > 0)
+		 x++;
+		System.out.println("x is " + x);
+	}
+
+}
