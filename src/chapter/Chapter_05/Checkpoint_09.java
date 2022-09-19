ab6f17d91e8c365ca4db453066ceb3475a322e9e
nmesih <nmesihalan@gmail.com>
2022-09-19 19:49:43 +0300

    added checkpoint 9

---
+++ src/chapter/Chapter_05/Checkpoint_09.java
@@ -0,0 +1,12 @@
+package chapter.Chapter_05;
+
+public class Checkpoint_09 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+			
+		}
+	}
+
+}
