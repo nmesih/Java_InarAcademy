107018881308161830d407bbc289432b82e6a170
nmesih <111389866+nmesih@users.noreply.github.com>
2022-09-22 08:33:47 +0300

    Added exercise 08

---
+++ src/chapter/Chapter_05/Exercise_08.java
@@ -0,0 +1,26 @@
+package chapter.Chapter_05;
+
+import java.util.Scanner;
+
+public class Exercise_08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of students: ");
+        int a = input.nextInt();
+        int max = 1;
+        String maxStudent = "";
+        for (int i = 0; i < a; i++){
+            System.out.println("Enter students name and scores: ");
+            String name = input.next();
+            int score = input.nextInt();
+            if(max < score){
+                max = score;
+                maxStudent = name;
+            }
+        }
+        System.out.println("The student with the highest score is " + maxStudent);
+        System.out.println("The highest score is " + max);
+    }
+}
+
+
