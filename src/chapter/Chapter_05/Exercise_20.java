503d38c456e9613ec3884da6a651fee321ea93dc
nmesih <111389866+nmesih@users.noreply.github.com>
2022-09-23 15:05:45 +0300

    added exercise 20

---
+++ src/chapter/Chapter_05/Exercise_20.java
@@ -0,0 +1,33 @@
+package chapter.Chapter_05;
+
+public class Exercise_20 {
+
+	public static void main(String[] args) {
+		System.out.println("Prime numbers between 2 and 1000 are: \n");
+		int count = 0;
+		int number = 2;
+		int NUMBER_OF_PRIMES_PER_LINE = 8;
+
+		while (number < 1000) {
+			boolean isPrime = true;
+
+			for (int bolen = 2; bolen <= number / 2; bolen++) {
+				if (number % bolen == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) {
+				count++;
+
+				if (count % NUMBER_OF_PRIMES_PER_LINE == 0) {
+					System.out.printf("%4d\n" , number);
+				} else {
+					System.out.printf("%4d" , number);
+				}
+			}
+			number++;
+		}
+	}
+
+}
